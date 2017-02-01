ed78e29ead6224c14f2deea77064be6d9ab66f71
ferrugemjs <ferrugemjavascript@gmail.com>
2017-02-01 05:28:53 +0000

    test

--- src/example/todo-app/todo-item.ts
+++ src/example/todo-app/todo-item.ts
@@ -6,14 +6,24 @@ export class TodoItem implements ITodoItem{
 	public desc:string;
 	public done:boolean;
 	public creation:Date;
-	public onMarked:EventEmitter<{id:number,done:boolean}> = new EventEmitter();
+	public onMarked:{once:Function};
+	public _onMarked:EventEmitter<{id:number,done:boolean}> = new EventEmitter();
+	
+	constructor(){
+		this.onMarked = {once:(fn:Function)=>{
+			if(!this._onMarked.hasSubscribers()){
+				this._onMarked.once(<any>fn);
+			}
+		}}
+	}
+
 	private MarkAsDone():void{
-		console.log(this.onMarked);
+		console.log(this._onMarked);
 		this.done = !this.done;
-		this.onMarked.emit({id:this.id,done:this.done});
+		this._onMarked.emit({id:this.id,done:this.done});
 	}
 	private detached(){
 		console.log('hummmm hummmm')
-		this.onMarked.unsubscribeAll();
+		this._onMarked.unsubscribeAll();
 	}
 }
