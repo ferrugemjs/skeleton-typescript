b3751c42f603792372b86dde4dd165561fc1ef06
alexandre.araujo <alexandre.araujo@mv.com.br>
2017-02-01 12:40:50 -0300

    fixed todo example

--- src/example/todo-app/todo-item.ts
+++ src/example/todo-app/todo-item.ts
@@ -6,24 +6,18 @@ export class TodoItem implements ITodoItem{
 	public desc:string;
 	public done:boolean;
 	public creation:Date;
-	public onMarked:{once:Function};
-	public _onMarked:EventEmitter<{id:number,done:boolean}> = new EventEmitter();
+	public onMarked:EventEmitter<{id:number,done:boolean}> = new EventEmitter();
 	
 	constructor(){
-		this.onMarked = {once:(fn:Function)=>{
-			if(!this._onMarked.hasSubscribers()){
-				this._onMarked.once(<any>fn);
-			}
-		}}
 	}
 
 	private MarkAsDone():void{
-		console.log(this._onMarked);
+		//console.log(this.onMarked);
 		this.done = !this.done;
-		this._onMarked.emit({id:this.id,done:this.done});
+		this.onMarked.emit({id:this.id,done:this.done});
 	}
 	private detached(){
-		console.log('hummmm hummmm')
-		this._onMarked.unsubscribeAll();
+		//console.log('hummmm hummmm')
+		this.onMarked.unsubscribeAll();
 	}
 }
